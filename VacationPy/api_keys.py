637abef50cc8c02f0d17d2edba7d3c970ca5c2f8
Mario Martinez <mvnord_ss@hotmail.com>
2020-05-12 18:54:01 -0500

    Second  Commitment

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,2 +1,2 @@
 # Google API Key
-g_key = "AIzaSyDvnm6cfW658axOVFBCeDZDlAk22KJ7tmU"
+g_key = ""
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,3 +1,3 @@
 # OpenWeatherMap API Key
-weather_api_key = "a647e53c657ff6a5d9daad267bed2618"
+weather_api_key = ""
 
